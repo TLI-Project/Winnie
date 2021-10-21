2e26112325d678e4e65b714cc5c3d6d9b37f315d
Callum Sharrock <38599348+callum-sh@users.noreply.github.com>
2021-10-21 01:37:22 -0400

    fixing fetch for tech demo. Shows up on screen

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
-import { useEffect } from 'react';
+import {useEffect, useState} from 'react';
 
 function App() {
   const baseEndpoint = 'http://piglet-env.eba-u7t4ntpp.us-east-2.elasticbeanstalk.com'
-  const loginEndpoint = '/login'
+  const loginEndpoint = '/mockSensoAPI'
+  const [data, setData] = useState('');
+  const [isLoaded, setIsLoaded] = useState(false);
+
   useEffect(() => {
     fetch(`${baseEndpoint}${loginEndpoint}`)
-      .then(res => res.json())
-      .then(console.log)
-      .catch(error => alert('Error! ' + error.message))
+        .then(res => res.text())
+        .then(
+            (result) => {
+              setIsLoaded(true);
+              setData(result);
+            },
+            // Note: it's important to handle errors here
+            // instead of a catch() block so that we don't swallow
+            // exceptions from actual bugs in components.
+            (error) => {
+              setIsLoaded(true);
+            }
+        )
   }, [])
+
   return (
     <div className="App">
+      {isLoaded?data:'loading...'}
     </div>
   );
 }
