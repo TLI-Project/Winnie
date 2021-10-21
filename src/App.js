ef8e970ae5fca2bdf64c30b909068326673a89ad
Joanne Pan <jpan0917@gmail.com>
2021-10-21 00:46:21 -0400

    ping endpoints

--- src/App.js
+++ src/App.js
@@ -1,4 +1,14 @@
+import { useEffect } from 'react';
+
 function App() {
+  const baseEndpoint = 'http://piglet-env.eba-u7t4ntpp.us-east-2.elasticbeanstalk.com'
+  const loginEndpoint = '/login'
+  useEffect(() => {
+    fetch(`${baseEndpoint}${loginEndpoint}`)
+      .then(res => res.json())
+      .then(console.log)
+      .catch(error => alert('Error! ' + error.message))
+  }, [])
   return (
     <div className="App">
     </div>
