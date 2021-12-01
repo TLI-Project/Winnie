3c2822dc3bba48cf12a4bfb72a0e335280ba24f9
Joanne Pan <71951015+jojoonthat@users.noreply.github.com>
2021-12-01 14:20:57 -0500

    testing database

--- src/components/Catalogue/index.js
+++ src/components/Catalogue/index.js
@@ -1,23 +1,57 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link, useHistory } from 'react-router-dom';
 import blueEllipse from '../../img/landing/background/Ellipse-blue.png';
 import greenEllipse from '../../img/landing/background/Ellipse-green.png';
 
 const Selection = () => {
-    const [carOne, setCarOne] = useState('')
-    const [carTwo, setCarTwo] = useState('')
-    const [carThree, setCarThree] = useState('')
-    const [carFour, setCarFour] = useState('')
+    const [selected, setSelected] = useState([])
     const history = useHistory();
+
+    const [processedData, setProcessedData] = useState(null);
+
+    useEffect(() => {
+        const fetchCarMeta = async () => {
+            const res = await fetch('http://localhost:8080/carMetaData', requestOptions);
+            const data = await res.json();
+            console.log(data)
+            const processed = []
+            for (let i = 1; i < 12; i++) {
+                processed.push(JSON.parse(data[i]))
+            }
+            setProcessedData(processed)
+        }
+        fetchCarMeta();
+    }, [])
+
+
 
     const submit = () => {
         // fetch to verify, if ok, history.push
-        if (carOne == '') {
+        if (selected.length == 0) {
             alert('please select a car')
         } else {
             history.push('/compare')
         }
     }
+
+
+    const handleAddCar = id => {
+        if (selected.includes(id)) {
+            setSelected(selected.filter(x => x !== id));
+        } else {
+            if (selected.length === 4) {
+                alert('no')
+            } else {
+                setSelected(selected.concat([id]))
+            }
+        }
+    }
+
+
+    const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+    };
 
     return (
         <div className="page page-catalogue">
@@ -25,22 +59,28 @@ const Selection = () => {
             <img className="ellipse-green" src={greenEllipse} />
             <h1> Browse for your desired vehicle</h1>
             <div className="container">
+                {processedData !== null &&
+                    processedData.map(entry => (
+                        <div className='gallery'>
+                            <p>{selected.includes(entry.id) ? 'SELECTED' : 'NOT SELECTED'}</p>
+                            <Link to={`/details/${entry.id}`}>
+                                <img src={entry.imageURL} alt={entry.carModel} />
+                            </Link>
+                            <div className="desc">Add a description of the image here</div>
+                            <button onClick={() => handleAddCar(entry.id)}></button>
+                        </div>
+                    ))
+                }
                 <div className="gallery">
-                    <a target="_blank" href="https://images.dealer.com/ddc/vehicles/2022/BMW/228%20Gran%20Coupe/Sedan/trim_i_sDrive_fb2469/color/Alpine%20White-300-233%2C238%2C232-640-en_US.jpg?impolicy=downsize_bkpt&imdensity=1&w=520">
-                        <Link to="/details">
-                            <img src="https://images.dealer.com/ddc/vehicles/2022/BMW/228%20Gran%20Coupe/Sedan/trim_i_sDrive_fb2469/color/Alpine%20White-300-233%2C238%2C232-640-en_US.jpg?impolicy=downsize_bkpt&imdensity=1&w=520" alt="Cinque Terre" width="600" height="400">
-                            </img>
-                        </Link>
-                    </a>
-                    <div className="desc">Add a description of the image here</div>
+
                 </div>
                 <div className="gallery">
-                    <a target="_blank" href="https://images.dealer.com/ddc/vehicles/2022/BMW/228%20Gran%20Coupe/Sedan/trim_i_sDrive_fb2469/color/Alpine%20White-300-233%2C238%2C232-640-en_US.jpg?impolicy=downsize_bkpt&imdensity=1&w=520">
-                        <Link to="/details">
-                            <img src="https://images.dealer.com/ddc/vehicles/2022/BMW/228%20Gran%20Coupe/Sedan/trim_i_sDrive_fb2469/color/Alpine%20White-300-233%2C238%2C232-640-en_US.jpg?impolicy=downsize_bkpt&imdensity=1&w=520" alt="Cinque Terre" width="600" height="400">
-                            </img>
-                        </Link>
-                    </a>
+                    {/* <a target="_blank" href="https://images.dealer.com/ddc/vehicles/2022/BMW/228%20Gran%20Coupe/Sedan/trim_i_sDrive_fb2469/color/Alpine%20White-300-233%2C238%2C232-640-en_US.jpg?impolicy=downsize_bkpt&imdensity=1&w=520"> */}
+                    <Link to="/details">
+                        <img src="https://images.dealer.com/ddc/vehicles/2022/BMW/228%20Gran%20Coupe/Sedan/trim_i_sDrive_fb2469/color/Alpine%20White-300-233%2C238%2C232-640-en_US.jpg?impolicy=downsize_bkpt&imdensity=1&w=520" alt="Cinque Terre" width="600" height="400">
+                        </img>
+                    </Link>
+                    {/* </a> */}
                     <div className="desc">Add a description of the image here</div>
                 </div>
                 <div className="gallery">
@@ -53,7 +93,7 @@ const Selection = () => {
                     <div className="desc">Add a description of the image here</div>
                 </div>
             </div>
-                <button className="btn" onClick={submit}>Next</button>
+            <button className="btn" onClick={submit}>Next</button>
         </div>
     )
 }
--- src/components/Details/index.js
+++ src/components/Details/index.js
@@ -1,9 +1,58 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './details.scss';
+import { useParams } from 'react-router';
 /*Needs car dataset to display the pictures, price points, model and name etc*/
 
 const Details = () => {
+    const { carId } = useParams()
+    console.log(carId)
+
+    const [carData, setCarData] = useState(null)
+    const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: parseInt(carId)
+    };
+
+    useEffect(() => {
+        // fetch data
+
+        const fetchCarData = async () => {
+            const res = await fetch('http://localhost:8080/carDetails', requestOptions);
+            const data = await res.json();
+            console.log(data)
+            // set data as state
+            const carData = {
+                carId: data.carId,
+                carModel: data.carModel,
+                carMake: data.carMake,
+                price: data.listPrice,
+                year: data.year,
+                kms: data.kms,
+                color: data.color,
+                condition: data.condition,
+                imageUrl: data.imageURL,
+                features: data.features
+        // this.id = id;
+        // this.carModel = carModel;
+        // this.carMake = carMake;
+        // this.listPrice = listPrice;
+        // this.year = year;
+        // this.kms = kms;
+        // this.color = color;
+        // this.condition = condition;
+        // this.depreciation = depreciation;
+        // this.imageURL = imageURL;
+        // this.features = features;
+            }
+            setCarData(carData)
+            console.log(carData)
+        }
+        fetchCarData();
+    }, [])
+
     return (
+        // render if data available
         <div className="Details">
             <main className="container">
                 <div className="left-column">
