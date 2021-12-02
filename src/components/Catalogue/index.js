673241b8cf2abe5f17ebf7c947d30441a297fafb
Joanne Pan <71951015+jojoonthat@users.noreply.github.com>
2021-12-01 22:00:47 -0500

    connected frontend to backend on /details/ page and /catalogue page

--- src/components/Catalogue/index.js
+++ src/components/Catalogue/index.js
@@ -15,9 +15,10 @@ const Selection = () => {
             const data = await res.json();
             console.log(data)
             const processed = []
-            for (let i = 1; i < 12; i++) {
+            for (let i = 1; i < 13; i++) {
                 processed.push(JSON.parse(data[i]))
             }
+            console.log(processed)
             setProcessedData(processed)
         }
         fetchCarMeta();
@@ -57,41 +58,24 @@ const Selection = () => {
         <div className="page page-catalogue">
             <img className="ellipse-blue" src={blueEllipse} />
             <img className="ellipse-green" src={greenEllipse} />
-            <h1> Browse for your desired vehicle</h1>
+            <h1>Browse for your desired vehicle</h1>
             <div className="container">
                 {processedData !== null &&
                     processedData.map(entry => (
-                        <div className='gallery'>
-                            <p>{selected.includes(entry.id) ? 'SELECTED' : 'NOT SELECTED'}</p>
+                        <div className='gallery' key={entry.id + entry.carMake + entry.carModel}>
+                            {selected.includes(entry.id) &&
+                                <div className='checkmark'>✔️</div>}
                             <Link to={`/details/${entry.id}`}>
                                 <img src={entry.imageURL} alt={entry.carModel} />
                             </Link>
-                            <div className="desc">Add a description of the image here</div>
-                            <button onClick={() => handleAddCar(entry.id)}></button>
+                            <h2 className="name">{`${entry.year} ${entry.carMake} ${entry.carModel}`}</h2>
+                            <div className='price-btn-container'>
+                                <p className='price'>{entry.listPrice}</p>
+                                <button onClick={() => handleAddCar(entry.id)}>Select</button>
+                            </div>
                         </div>
                     ))
                 }
-                <div className="gallery">
-
-                </div>
-                <div className="gallery">
-                    {/* <a target="_blank" href="https://images.dealer.com/ddc/vehicles/2022/BMW/228%20Gran%20Coupe/Sedan/trim_i_sDrive_fb2469/color/Alpine%20White-300-233%2C238%2C232-640-en_US.jpg?impolicy=downsize_bkpt&imdensity=1&w=520"> */}
-                    <Link to="/details">
-                        <img src="https://images.dealer.com/ddc/vehicles/2022/BMW/228%20Gran%20Coupe/Sedan/trim_i_sDrive_fb2469/color/Alpine%20White-300-233%2C238%2C232-640-en_US.jpg?impolicy=downsize_bkpt&imdensity=1&w=520" alt="Cinque Terre" width="600" height="400">
-                        </img>
-                    </Link>
-                    {/* </a> */}
-                    <div className="desc">Add a description of the image here</div>
-                </div>
-                <div className="gallery">
-                    <a target="_blank" href="https://images.dealer.com/ddc/vehicles/2022/BMW/228%20Gran%20Coupe/Sedan/trim_i_sDrive_fb2469/color/Alpine%20White-300-233%2C238%2C232-640-en_US.jpg?impolicy=downsize_bkpt&imdensity=1&w=520">
-                        <Link to="/details">
-                            <img src="https://images.dealer.com/ddc/vehicles/2022/BMW/228%20Gran%20Coupe/Sedan/trim_i_sDrive_fb2469/color/Alpine%20White-300-233%2C238%2C232-640-en_US.jpg?impolicy=downsize_bkpt&imdensity=1&w=520" alt="Cinque Terre" width="600" height="400">
-                            </img>
-                        </Link>
-                    </a>
-                    <div className="desc">Add a description of the image here</div>
-                </div>
             </div>
             <button className="btn" onClick={submit}>Next</button>
         </div>
--- src/components/Details/index.js
+++ src/components/Details/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, Fragment } from 'react'
 import './details.scss';
 import { useParams } from 'react-router';
 /*Needs car dataset to display the pictures, price points, model and name etc*/
@@ -33,17 +33,6 @@ const Details = () => {
                 condition: data.condition,
                 imageUrl: data.imageURL,
                 features: data.features
-        // this.id = id;
-        // this.carModel = carModel;
-        // this.carMake = carMake;
-        // this.listPrice = listPrice;
-        // this.year = year;
-        // this.kms = kms;
-        // this.color = color;
-        // this.condition = condition;
-        // this.depreciation = depreciation;
-        // this.imageURL = imageURL;
-        // this.features = features;
             }
             setCarData(carData)
             console.log(carData)
@@ -55,42 +44,32 @@ const Details = () => {
         // render if data available
         <div className="Details">
             <main className="container">
-                <div className="left-column">
-                    <img src="https://affinitycarrentals.com/wp-content/uploads/bfi_thumb/MDX-p5ohmlyhg58ums62auoyx0gh32olvo8f4qs7xbxhno.png">
-                    </img>
-                </div>
-
-                <div className="right-column">
-                    <div className="product-description">
-                        <span>Ford Car</span>
-                        <h1>FORD Focus</h1>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-                            magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
-                            ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                            fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-                            mollit anim id est laborum. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                            fugiat nulla pariatur.</p>
-                    </div>
-                    <div className="product-configuration">
-                        <div className="product-color">
-                            <span>Color</span>
+                {carData !== null &&
+                    <Fragment>
+                        <div className="left-column">
+                            <img src={carData.imageUrl}>
+                            </img>
                         </div>
-                        <div className="cable-config">
-                            <span>Cable configuration</span>
 
-                            <div className="cable-choose">
-                                <button>Straight</button>
-                                <button>Coiled</button>
-                                <button>Long-coiled</button>
+                        <div className="right-column">
+                            <div className="product-description">
+                                <span>{carData.carMake}</span>
+                                <h1>{carData.carModel}</h1>
+                                <p>{carData.features}</p>
+                            </div>
+                        
+
+                            <div className="product-price">
+                                <span>${carData.price}</span>
                             </div>
                         </div>
-                    </div>
 
-                    <div className="product-price">
-                        <span>148$</span>
-                        <a href="#" className="cart-btn">Add to cart</a>
-                    </div>
-                </div>
+                    </Fragment>
+                }
+
+
+
+
             </main>
         </div>
     )
