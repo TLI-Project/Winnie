834470412fc7a568dfe640537147063d757f3813
annaa-cho <ac.anna.cho@gmail.com>
2021-12-01 21:37:13 -0500

    finish advantages block and half complete footer

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -16,6 +16,18 @@ import StepsLeftBg from '../../img/landing/steps/left-background.png';
 import StepsCenterBg from '../../img/landing/steps/center-background.png';
 import StepsRightBg from '../../img/landing/steps/right-background.png';
 
+import AdvantagesCar from '../../img/landing/advantages/car.png';
+import Temp2 from '../../img/landing/advantages/icon.png';
+
+import Facebook from '../../img/landing/footer/fb-icon.png';
+import Instagram from '../../img/landing/footer/ig-icon.png';
+import LinkedIn from '../../img/landing/footer/li-icon.png';
+import Twitter from '../../img/landing/footer/twitter-icon.png';
+import FooterBg from '../../img/landing/footer/background.png';
+import Location from '../../img/landing/footer/location.png';
+import Phone from '../../img/landing/footer/phone.png';
+import Email from '../../img/landing/footer/email.png';
+
 const Home = () => {
     const history = useHistory();
     const submitBuyer = () => {
@@ -129,7 +141,7 @@ const Home = () => {
                 
                 <div className="steps__grid">
                     <div className="steps__grid-item">
-                        <img className='steps__grid-img' alt="" src={Temp}/>
+                        <img className='steps__grid-item-img' alt="" src={Temp}/>
                         <div className='steps__grid-info'>
                             <h3 className='steps__grid-info-title'>lorem ipsum dolor sit amet</h3>
                             <h3 className='steps__grid-info-detail'>Lorem ipsum dolor sit amet, consectetur.</h3>
@@ -140,7 +152,7 @@ const Home = () => {
                     </div>
 
                     <div className="steps__grid-item">
-                        <img className='steps__grid-img' alt="" src={Temp}/>
+                        <img className='steps__grid-item-img' alt="" src={Temp}/>
                         <div className='steps__grid-info'>
                             <h3 className='steps__grid-info-title'>lorem ipsum dolor sit amet</h3>
                             <h3 className='steps__grid-info-detail'>Lorem ipsum dolor sit amet, consectetur.</h3>
@@ -151,7 +163,7 @@ const Home = () => {
                     </div>
 
                     <div className="steps__grid-item">
-                        <img className='steps__grid-img' alt="" src={Temp}/>
+                        <img className='steps__grid-item-img' alt="" src={Temp}/>
                         <div className='steps__grid-info'>
                             <h3 className='steps__grid-info-title'>lorem ipsum dolor sit amet</h3>
                             <h3 className='steps__grid-info-detail'>Lorem ipsum dolor sit amet, consectetur.</h3>
@@ -162,13 +174,94 @@ const Home = () => {
                     </div>
 
                     <div className="steps__grid-item">
-                        <img className='steps__grid-img' alt="" src={Temp}/>
+                        <img className='steps__grid-item-img' alt="" src={Temp}/>
                         <div className='steps__grid-info'>
                             <h3 className='steps__grid-info-title'>lorem ipsum dolor sit amet</h3>
                             <h3 className='steps__grid-info-detail'>Lorem ipsum dolor sit amet, consectetur.</h3>
                         </div>
                         <div className='steps__number'>
                             <p className='steps__number-fig'>4</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="advantages">
+                <img className='advantages__img' src={AdvantagesCar}></img>
+                <div className='advantages__text'>
+                    <h2 className="subtitle" id="advantages__text-title">Why Senso Education?</h2>
+                    <p className="detail" id="advantages__text-detail">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    </p>
+                </div>
+
+                <div className="advantages__grid">
+                    <div className="advantages__grid-item">
+                        <img className='advantages__grid-item-img' alt="" src={Temp2}/>
+                        <div className='advantages__grid-info'>
+                            <h3 className='advantages__grid-info-title'>Lorem Ipsum</h3>
+                            <h3 className='advantages__grid-info-detail'>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididuntut lab.</h3>
+                        </div>
+                    </div>
+
+                    <div className="advantages__grid-item">
+                        <img className='advantages__grid-item-img' alt="" src={Temp2}/>
+                        <div className='advantages__grid-info'>
+                            <h3 className='advantages__grid-info-title'>Lorem Ipsum</h3>
+                            <h3 className='advantages__grid-info-detail'>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididuntut lab.</h3>
+                        </div>
+                    </div>
+
+                    <div className="advantages__grid-item">
+                        <img className='advantages__grid-item-img' alt="" src={Temp2}/>
+                        <div className='advantages__grid-info'>
+                            <h3 className='advantages__grid-info-title'>Lorem Ipsum</h3>
+                            <h3 className='advantages__grid-info-detail'>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididuntut lab.</h3>
+                        </div>
+                    </div>
+
+                    <div className="advantages__grid-item">
+                        <img className='advantages__grid-item-img' alt="" src={Temp2}/>
+                        <div className='advantages__grid-info'>
+                            <h3 className='advantages__grid-info-title'>Lorem Ipsum</h3>
+                            <h3 className='advantages__grid-info-detail'>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididuntut lab.</h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div id='footer-container'>
+                <div id='socials'>
+                    <p className='connect'>connect with us!</p>
+                    <img className='socials-icon' alt="linkedin" src={LinkedIn}/>
+                    <img className='socials-icon' alt="instagram" src={Instagram}/>
+                    <img className='socials-icon' alt="facebook" src={Facebook}/>
+                    <img className='socials-icon' alt="twitter" src={Twitter}/>
+                </div>
+                <div className='footer'>
+                    <img className='footer-img' alt="footer" src={FooterBg}/>
+                    <div id="footer-description">
+                        <div id='footer-main'>
+                            <h2 id="footer-title">Footer</h2>
+                            <p id="footer-info">Lorem ipsum dolor sit amet, consectetur adipisicing elit, 
+                            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, 
+                            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                        </div>
+                        <div id="footer-contact">
+                            <div className='contacts-container'>
+                                <img className='contacts-icon' alt="location" src={Location}/>
+                                <p className='contacts'>123 Wallaby Way</p>
+                            </div>
+                            <div className='contacts-container'>
+                                <img className='contacts-icon' alt="phone" src={Phone}/>
+                                <p className='contacts'>123-456-7890</p>
+                            </div>
+                            <div className='contacts-container'>
+                                <img className='contacts-icon' alt="email" src={Email}/>
+                                <p className='contacts'>info@form.com</p>
+                            </div>
                         </div>
                     </div>
                 </div>
