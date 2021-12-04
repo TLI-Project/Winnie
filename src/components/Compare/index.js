06a8d6a706a0f2196582595b563f9f548cadb27c
sophiaabolore <59152413+sophiaabolore@users.noreply.github.com>
2021-12-04 12:01:00 -0500

    Added rough draft for graph interpreatations and instructions

--- src/components/Compare/index.js
+++ src/components/Compare/index.js
@@ -10,7 +10,20 @@ const Compare = () => {
         <div className="page page-compare">
             <img className="ellipse-blue" src={blueEllipse} />
             <img className="ellipse-green" src={greenEllipse} />
-            return <Graph1Model newModelA = {{"listPrice": 27600, "model": "Honda CR-V",
+            <div className='detail-right'>
+                <h2>Interpreting the Graph</h2>
+                <p>View the Ammoritization graph: In order to understand what your monthly payments will look like
+                    and how much of it will be going towards your interest and how much will be going towards your
+                    capital. </p>
+                <p>View the Depreciation graph: In order to understand what the resale value of your vehicle will be
+                    after a specific amount of time. For example, if you plan to sell your vehicle after 5 years of
+                    owning it is important to know how much value each model will lose and how much you will be able to
+                resell each for</p>
+                <h2>Instructions</h2>
+                <p>Toggle on each model to remove it's data from the graph </p>
+                <p>Hover above datapoints (circles on the graph) for further insights</p>
+            </div>
+           <Graph1Model newModelA = {{"listPrice": 27600, "model": "Honda CR-V",
             "depreciation": [0.78, 0.73, 0.72, 0.69, 0.61, 0.53, 0.44, 0.44, 0.38, 0.34]}} usedModelA = {{"listPrice":  27600, "model": "Honda Clarity",
             "depreciation": [0.86, 0.81, 0.78, 0.75, 0.72, 0.71, 0.69, 0.65, 0.61, 0.55]}} />
 
